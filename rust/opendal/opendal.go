9bf91907081bdec26d6cece07566928909fd4f57
spongehah <2635879218@qq.com>
2024-07-11 18:06:02 +0800

    llgo/rust/opendal:error_handle.demo

--- rust/opendal/opendal.go
+++ rust/opendal/opendal.go
@@ -10,21 +10,76 @@ const (
 	LLGoPackage = "link: $(pkg-config --libs opendal_c); -lopendal_c"
 )
 
+type Code c.Int
+
+/**
+ * \brief The error code for all opendal APIs in C binding.
+ * \todo The error handling is not complete, the error with error message will be
+ * added in the future.
+ */
+const (
+	/*
+	 * returning it back. For example, s3 returns an internal service error.
+	 */
+	Unexpected Code = iota
+	/*
+	 * Underlying service doesn't support this operation.
+	 */
+	Unsupported
+	/*
+	 * The config for backend is invalid.
+	 */
+	ConfigInvalid
+	/*
+	 * The given path is not found.
+	 */
+	NotFound
+	/*
+	 * The given path doesn't have enough permission for this operation
+	 */
+	PermissionDenied
+	/*
+	 * The given path is a directory.
+	 */
+	IsADirectory
+	/*
+	 * The given path is not a directory.
+	 */
+	NotADirectory
+	/*
+	 * The given path already exists thus we failed to the specified operation on it.
+	 */
+	AlreadyExists
+	/*
+	 * Requests that sent to this path is over the limit, please slow down.
+	 */
+	RateLimited
+	/*
+	 * The given file paths are same.
+	 */
+	IsSameFile
+	/*
+	 * The condition of this operation is not match.
+	 */
+	ConditionNotMatch
+	/*
+	 * The range of the content is not satisfied.
+	 */
+	RangeNotSatisfied
+)
+
 type ResultOperator struct {
 	Op    *Operator
 	Error *Error
 }
 
 type OperatorOptions struct {
-	Unused [8]byte
+	Inner *HashMapStringString
 }
 
 type Error struct {
-	Unused [8]byte
-}
-
-type Operator struct {
-	Unused [8]byte
+	Code    Code
+	Message Bytes
 }
 
 type Bytes struct {
@@ -37,17 +92,88 @@ type ResultRead struct {
 	Error *Error
 }
 
+type BlockingLister struct {
+	Unused [8]byte
+}
+
+type BlockingOperator struct {
+	Unused [8]byte
+}
+
+type Operator struct {
+	Ptr *BlockingOperator
+}
+
+type InnerEntry struct {
+	Unused [8]byte
+}
+
+type HashMapStringString struct {
+	Unused [8]byte
+}
+
+type InnerMetadata struct {
+	Unused [8]byte
+}
+
+type OperatorInfo struct {
+	Unused [8]byte
+}
+
+type StdReader struct {
+	Unused [8]byte
+}
+
+type Entry struct {
+	Inner *InnerEntry
+}
+
+type ResultListerNext struct {
+	Entry *Entry
+	Error *Error
+}
+
+type Lister struct {
+	Inner *BlockingLister
+}
+
+type MetaData struct {
+	Inner *InnerMetadata
+}
+
+type Reader struct {
+	Inner *StdReader
+}
+
+type ResultOperatorReader struct {
+	Reader *Reader
+	Error  *Error
+}
+
+type ResultIsExist struct {
+	IsExist int8
+	Error   *Error
+}
+
+type ResultStat struct {
+	Meta  *MetaData
+	Error *Error
+}
+
 // llgo:link NewResultOperator C.opendal_operator_new
 func NewResultOperator(schema *c.Char, options *OperatorOptions) (rst ResultOperator) { return }
 
-// llgo:link (*Operator).OperatorWrite C.opendal_operator_write
-func (op *Operator) OperatorWrite(path *c.Char, bytes Bytes) *Error { return nil }
+// llgo:link (*Operator).Write C.opendal_operator_write
+func (op *Operator) Write(path *c.Char, bytes Bytes) *Error { return nil }
 
-// llgo:link (*Operator).OperatorRead C.opendal_operator_read
-func (op *Operator) OperatorRead(path *c.Char) (rst ResultRead) { return }
+// llgo:link (*Operator).Read C.opendal_operator_read
+func (op *Operator) Read(path *c.Char) (rst ResultRead) { return }
 
-// llgo:link (*Bytes).FreeBytes C.opendal_bytes_free
-func (bs *Bytes) FreeBytes() {}
+// llgo:link (*Bytes).Free C.opendal_bytes_free
+func (bs *Bytes) Free() {}
 
-// llgo:link (*Operator).FreeOperator C.opendal_operator_free
-func (op *Operator) FreeOperator() {}
+// llgo:link (*Operator).Free C.opendal_operator_free
+func (op *Operator) Free() {}
+
+// llgo:link (*Error).Free C.opendal_error_free
+func (err *Error) Free() {}
