fa17c44977ee5fd7695befef335d3e851106a2bc
2458zhanghehe <2458050004@qq.com>
2024-07-17 11:46:47 +0800

    feat(llgoexamples/cpp/spdlog): Use LLGoFile method

--- cpp/spdlog/_demo/Demo1.go
+++ cpp/spdlog/_demo/Demo1.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	spdlog.SpdlogPrintInfo(c.Str("Hello World"))
-	spdlog.SpdlogPrintCritical(c.Str("This is a critical message"))
-	spdlog.SpdlogPrintError(c.Str("This is an error message"))
+	spdlog.PrintInfo(c.Str("Hello World"))
+	spdlog.PrintCritical(c.Str("This is a critical message"))
+	spdlog.PrintError(c.Str("This is an error message"))
 	//spdlog.Shutdown()
-	spdlog.SpdlogPrintWarn(c.Str("This is a warning message"))
+	spdlog.PrintWarn(c.Str("This is a warning message"))
 	/*
 	*SPDLOG_LEVEL_TRACE 0
 	*SPDLOG_LEVEL_DEBUG 1
@@ -20,8 +20,8 @@ func main() {
 	*SPDLOG_LEVEL_CRITICAL 5
 	*SPDLOG_LEVEL_OFF 6
 	 */
-	spdlog.SpdlogPrintDebug(c.Str("This debug message should not be printed"))
-	spdlog.SpdlogSetLevel(1)
-	spdlog.SpdlogPrintDebug(c.Str("This debug messagen should be printed"))
-	spdlog.SpdlogPrintInfoWithInt(c.Str("Support for int :{}"), 100)
+	spdlog.PrintDebug(c.Str("This debug message should not be printed"))
+	spdlog.SetLevel(1)
+	spdlog.PrintDebug(c.Str("This debug messagen should be printed"))
+	spdlog.PrintInfoWithInt(c.Str("Support for int :{}"), 100)
 }
