09cd8fe8873df5894a0e0e7285af3c280933e1c8
hackerchai <i@hackerchai.com>
2024-09-03 19:27:10 +0800

    refactor(x/net/http/demo): Update http demo
    Signed-off-by: hackerchai <i@hackerchai.com>

--- x/net/http/_demo/http.go
+++ x/net/http/_demo/http.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"fmt"
-	"io"
+	//"io"
 
 	"github.com/goplus/llgo/x/net/http"
 )
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-	//defer r.Body.Close()
+	fmt.Printf("echoHandler called\n")
+	//TODO: read body and echo
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	http.Error(w, "Error reading request body", http.StatusInternalServerError)
+	// 	return
+	// }
+	// defer r.Body.Close()
+	// fmt.Printf("body: %s\n", string(body))
+	//w.Header().Set("Content-Type", "text/plain")
+	//w.Write(body)
 
 	w.Header().Set("Content-Type", "text/plain")
-
-	w.Write(body)
+	w.Write([]byte("echoHandler called\n"))
 }
 
 func main() {
